586a12a9a007648321a4aed77fe9f9acf0f5896f
cileene <106276740+cileene@users.noreply.github.com>
2026-04-25 00:45:25 +0200

    Add ui for param value display and keyboard input

--- aa_godot_viz/scripts/UIController.cs
+++ aa_godot_viz/scripts/UIController.cs
@@ -5,10 +5,15 @@ namespace aa_godot_viz.scripts;
 public partial class UIController : VBoxContainer
 {
 	[Export] private HSlider _colorSlider;
+	[Export] private SpinBox _colorValue;
 	[Export] private HSlider _spatialitySlider;
+	[Export] private SpinBox _spatialityValue;
 	[Export] private HSlider _compositionSlider;
+	[Export] private SpinBox _compositionValue;
 	[Export] private HSlider _shapeSlider;
+	[Export] private SpinBox _shapeValue;
 	[Export] private HSlider _materialSlider;
+	[Export] private SpinBox _materialValue;
 
 	[Export] public bool ShowFps = true;
 
@@ -17,11 +22,11 @@ public partial class UIController : VBoxContainer
 
 	public override void _Ready()
 	{
-		_colorSlider.ValueChanged += v => { _color = (float)v; RaiseParameters(); };
-		_spatialitySlider.ValueChanged += v => { _spatiality = (float)v; RaiseParameters(); };
-		_compositionSlider.ValueChanged += v => { _composition = (float)v; RaiseParameters(); };
-		_shapeSlider.ValueChanged += v => { _shape = (float)v; RaiseParameters(); };
-		_materialSlider.ValueChanged += v => { _material = (float)v; RaiseParameters(); };
+		BindSlider(_colorSlider,      _colorValue,      v => { _color = v;       RaiseParameters(); });
+		BindSlider(_spatialitySlider, _spatialityValue, v => { _spatiality = v;  RaiseParameters(); });
+		BindSlider(_compositionSlider,_compositionValue,v => { _composition = v; RaiseParameters(); });
+		BindSlider(_shapeSlider,      _shapeValue,      v => { _shape = v;       RaiseParameters(); });
+		BindSlider(_materialSlider,   _materialValue,   v => { _material = v;    RaiseParameters(); });
 
 		_fpsLabel = new Label();
 		_fpsLabel.SetAnchorsPreset(LayoutPreset.TopRight);
@@ -31,6 +36,26 @@ public partial class UIController : VBoxContainer
 		GetParent().CallDeferred("add_child", _fpsLabel);
 
 		RaiseParameters();
+	}
+
+	private void BindSlider(HSlider slider, SpinBox spinBox, System.Action<float> onChange)
+	{
+		spinBox.MinValue = slider.MinValue;
+		spinBox.MaxValue = slider.MaxValue;
+		spinBox.Step = slider.Step > 0 ? slider.Step : 0.01;
+		spinBox.Value = slider.Value;
+
+		slider.ValueChanged += v =>
+		{
+			onChange((float)v);
+			spinBox.SetValueNoSignal(v);
+		};
+
+		spinBox.ValueChanged += v =>
+		{
+			slider.SetValueNoSignal(v);
+			onChange((float)v);
+		};
 	}
 
 	public override void _ExitTree()
