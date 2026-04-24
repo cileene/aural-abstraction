285867958c591c0caad1a53d67e6823e69de553d
cileene <106276740+cileene@users.noreply.github.com>
2026-04-24 10:01:18 +0200

    Add FPS display toggle and refine scene lighting/geometry adjustments

--- aa_godot_viz/scripts/UIController.cs
+++ aa_godot_viz/scripts/UIController.cs
@@ -9,8 +9,11 @@ public partial class UIController : VBoxContainer
 	[Export] private HSlider _compositionSlider;
 	[Export] private HSlider _shapeSlider;
 	[Export] private HSlider _materialSlider;
-	
+
+	[Export] public bool ShowFps = true;
+
 	private float _color = 0.5f, _spatiality = 0.5f, _composition = 0.5f, _shape = 0.5f, _material = 0.5f;
+	private Label _fpsLabel;
 
 	public override void _Ready()
 	{
@@ -20,7 +23,27 @@ public partial class UIController : VBoxContainer
 		_shapeSlider.ValueChanged += v => { _shape = (float)v; RaiseParameters(); };
 		_materialSlider.ValueChanged += v => { _material = (float)v; RaiseParameters(); };
 
+		_fpsLabel = new Label();
+		_fpsLabel.SetAnchorsPreset(LayoutPreset.TopRight);
+		_fpsLabel.GrowHorizontal = GrowDirection.Begin;
+		_fpsLabel.OffsetRight = -10;
+		_fpsLabel.OffsetTop = 10;
+		GetParent().CallDeferred("add_child", _fpsLabel);
+
 		RaiseParameters();
+	}
+
+	public override void _ExitTree()
+	{
+		_fpsLabel?.QueueFree();
+	}
+
+	public override void _Process(double delta)
+	{
+		if (_fpsLabel == null) return;
+		_fpsLabel.Visible = ShowFps;
+		if (ShowFps)
+			_fpsLabel.Text = $"{Engine.GetFramesPerSecond()} FPS";
 	}
 
 	private void RaiseParameters()
